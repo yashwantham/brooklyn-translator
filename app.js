db9dac24d37bfaf7a3841f80894836ed7bb71b48
Yashwanth <yashwanthyashu166@gmail.com>
2023-01-26 12:29:48 +0530

    done

---
+++ app.js
@@ -0,0 +1,29 @@
+var btnTranslate = document.querySelector("#btn-translate"); //reference to html element(button)
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/brooklyn.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("Server is down. Pls try again after some time");
+}
+
+function clickHandler() {
+  var inputText = txtInput.value; //taking input
+
+  // calling server for processing
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText; // output
+    })
+    .catch(errorHandler);
+}
+//Event listener
+btnTranslate.addEventListener("click", clickHandler);
